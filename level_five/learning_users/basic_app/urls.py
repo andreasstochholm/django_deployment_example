fee0aa46fb3c247544994cd041d49580fb72953b
andreasstochholm <andreasstochholm@gmail.com>
2020-10-23 06:42:22 +0200

    first commit

---
+++ level_five/learning_users/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from basic_app import views
+from django.urls import include, re_path, path
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    re_path(r'^register/$',views.register,name='register'),
+    re_path(r'^user_login/$',views.user_login, name='user_login'),
+
+]
